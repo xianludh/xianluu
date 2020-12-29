4fde48b52dbca7032181d8d28ba4cff9f10519d6
unpack <156332666@qq.com>
2020-12-29 18:32:16 +0800

    bet992

--- bet992/nav/m/js/speed.js
+++ bet992/nav/m/js/speed.js
@@ -1,11 +1,11 @@
 
-    var save=""; 
+  var save=""; 
 	var tim=1;  
 	setInterval("tim++",10);  
 	var b=1;  
 	var autourl=new Array();  
-	autourl[1]="//bb365v.com";autourl[2]="//bb365i.com";autourl[3]="//bb365h.com";autourl[4]="//bb365k.com";autourl[5]="//bb365f.com";autourl[6]="//bb365e.com"; autourl[7]="//bb365l.com";autourl[8]="//bb365c.com";autourl[9]="//bb365b.com";autourl[10]="//bb365w.com";
-	function auto(url,b){ 
+	autourl[1]="//b4177.com";autourl[2]="//b4200.com";autourl[3]="//b4022.com";autourl[4]="//b4266.com";autourl[5]="//b3349.com";
+    function auto(url,b){
 		$("#sudu"+b).html(tim+"ms");
 	}  
 	function run(){  
